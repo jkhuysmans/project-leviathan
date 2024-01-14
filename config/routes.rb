8562b93a8c55825289ea83c7cf71a695a286dd84
jkhuysmans <149975556+jkhuysmans@users.noreply.github.com>
2024-01-15 00:54:43 +0100

    json

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  namespace :api do
+  namespace :api, defaults: { format: :json } do
     namespace :v1 do
       get '/kline', to: 'api_data#kline'
       get '/openinterest', to: 'api_data#openinterest'
