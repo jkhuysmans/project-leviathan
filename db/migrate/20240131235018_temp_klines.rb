04f30178ec24aab466ba9364e9d125e5c55d8e56
jkhuysmans <149975556+jkhuysmans@users.noreply.github.com>
2024-02-02 09:26:41 +0100

    change db

--- db/migrate/20240131235018_temp_klines.rb
+++ db/migrate/20240131235018_temp_klines.rb
@@ -1,5 +1,9 @@
 class TempKlines < ActiveRecord::Migration[7.1]
-  def change
+  def up
     execute 'CREATE TABLE import_klines (LIKE klines INCLUDING ALL)'
+  end
+
+  def down
+    execute 'DROP TABLE IF EXISTS import_klines'
   end
 end
