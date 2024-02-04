18f2f48d3dee63ecc79509a70aa63e31051398d6
jkhuysmans <149975556+jkhuysmans@users.noreply.github.com>
2024-02-04 11:48:47 +0100

    re set cron jobs

--- config/schedule.rb
+++ config/schedule.rb
@@ -21,3 +21,10 @@
 
 env :PATH, ENV['PATH']
 
+every 1.minute do
+    rake 'klines_refresh:scratch_by_minute', environment: 'production'
+  end
+
+every 5.minute do
+  rake 'oi_fetcher:refresh_oi', environment: 'production'
+end
